e4a9670c1e54e87d2093bec12be03937722aad6a
Gyanesh Gouraw <gyanesh.gouraw@okta.com>
2025-02-03 09:47:35 +0530

    Added logger on login screen

--- src/screens/Login/hooks/useLoginManager.ts
+++ src/screens/Login/hooks/useLoginManager.ts
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import LoginInstance from "@auth0/auth0-acul-js/login";
+import { withWindowDebug } from "../../../utils";
 
 export const useLoginManager = () => {
   const [loginIdManager] = useState(() => new LoginInstance());
+  withWindowDebug(loginIdManager, 'login')
 
   const handleLogin = (username: string, password: string, captcha: string): void => {
     const options = {
