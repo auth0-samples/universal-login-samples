b823ce7735b49c84314b71a5f8c9bf7e91fae141
Nandan Bhat <nandan.bhat@okta.com>
2025-03-21 02:13:26 +0530

    WIP: Adding examples for customized-consent screen

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,7 @@ const ResetPasswordMfaOtpChallengeScreen = React.lazy(() => import("./screens/re
 const OrganizationSelectionScreen = React.lazy(() => import("./screens/organization-selection"));
 const OrganizationPickerScreen = React.lazy(() => import("./screens/organization-picker"));
 const AcceptInvitationScreen = React.lazy(() => import("./screens/accept-invitation"));
+const CustomizedConsentScreen = React.lazy(() => import("./screens/customized-consent"));
 
 
 const App: React.FC = () => {
@@ -111,6 +112,8 @@ const App: React.FC = () => {
         return <OrganizationPickerScreen />;
       case "accept-invitation":
         return <AcceptInvitationScreen />;
+      case "customized-consent":
+        return <CustomizedConsentScreen />;
       default:
         return <>No screen rendered</>;
     }
