cfb2c0ff60d11f8fa751190c0e0cbc0a906989bb
Nandan Bhat <nandan.bhat@okta.com>
2025-03-26 11:59:24 +0530

    Updating examples for organization screens

--- src/screens/organization-picker/index.tsx
+++ src/screens/organization-picker/index.tsx
@@ -3,7 +3,7 @@ import OrganizationPicker from '@auth0/auth0-acul-js/organization-picker';
 
 const OrganizationPickerScreen: React.FC = () => {
   const organizationPicker = new OrganizationPicker();
-  const { user } = organizationPicker;
+  const { screen, user } = organizationPicker;
 
   const handleSelectOrganization = async (organizationId: string) => {
     try {
@@ -22,9 +22,10 @@ const OrganizationPickerScreen: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center min-h-screen bg-gray-100">
       <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <h2 className="text-2xl font-bold mb-4">Select an Organization</h2>
+        <h2 className="text-2xl font-bold mb-4">{ `${ screen.texts?.title ?? 'Choose an Organization' }` }</h2>
+        <p>{ `${ user.email } ${ screen.texts?.loggedInText ?? 'is used as a personal account and is a part of multiple organizations.' }` } </p>
         {
           user.organizations ? (
             <ul className="mb-4">
@@ -47,7 +48,7 @@ const OrganizationPickerScreen: React.FC = () => {
           onClick={handleSkipOrganization}
           className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
         >
-          Continue with Personal Account
+          { `${ screen?.texts?.continueTextB2C ?? 'Continue with Personal Account' }` }
         </button>
       </div>
     </div>
