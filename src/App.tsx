b962dc3e5e54959990009076e8d603e389e9c4b8
Nandan Bhat <nandan.bhat@okta.com>
2025-05-08 02:02:00 +0530

    Added support for logout screen

--- src/App.tsx
+++ src/App.tsx
@@ -51,6 +51,7 @@ const MfaRecoveryCodeEnrollmentScreen = React.lazy(() => import("./screens/mfa-r
 const ResetPasswordMfaPhoneChallengeScreen = React.lazy(() => import("./screens/reset-password-mfa-phone-challenge"));
 const PasskeyEnrollmentScreen = React.lazy(() => import("./screens/passkey-enrollment"));
 const MfaRecoveryCodeChallengeNewCodeScreen = React.lazy(() => import("./screens/mfa-recovery-code-challenge-new-code"));
+const LogoutScreen = React.lazy(() => import("./screens/logout"));
 
 
 const App: React.FC = () => {
@@ -162,6 +163,8 @@ const App: React.FC = () => {
         return <PasskeyEnrollmentScreen />;
       case "mfa-recovery-code-challenge-new-code":
         return <MfaRecoveryCodeChallengeNewCodeScreen />;
+      case "logout":
+        return <LogoutScreen />;
       default:
         return <>No screen rendered</>;
     }
