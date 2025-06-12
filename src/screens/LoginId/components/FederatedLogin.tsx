ffaf365154abac93484ee7e74f6baeb3b1efb31f
Amit Singh <amitk.singh@okta.com>
2025-06-12 16:52:02 +0530

    Refactor: Replace SocialLogin component with FederatedLogin and update related methods

---
+++ src/screens/LoginId/components/FederatedLogin.tsx
@@ -0,0 +1,22 @@
+import Button from '../../../components/Button';
+interface Connection {
+  name: string;
+}
+
+interface FederatedLoginProps {
+  connections?: Connection[];
+  onFederatedLogin: (name: string) => void;
+}
+
+export const FederatedLogin: React.FC<FederatedLoginProps> = ({ connections, onFederatedLogin }) => (
+  <div className="federated-login-container">
+    {connections?.map((connection) => (
+      <Button
+        key={connection.name}
+        onClick={() => onFederatedLogin(connection.name)}
+      >
+        Continue withmm {connection.name}
+      </Button>
+    ))}
+  </div>
+); 
