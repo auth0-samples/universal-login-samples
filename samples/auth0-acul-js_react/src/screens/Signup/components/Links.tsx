02b3608165bada0da4a7f60732253a4233b26edd
Nandan Bhat <nandan.bhat@okta.com>
2025-09-11 17:20:28 +0530

    chore: fixed screen names in TS samples

--- samples/auth0-acul-js_react/src/screens/Signup/components/Links.tsx
+++
@@ -1,9 +0,0 @@
-interface LinksProps {
-  loginLink?: string;
-}
-
-export const Links: React.FC<LinksProps> = ({ loginLink }) => (
-  <div className="links">
-    {loginLink && <a href={loginLink}>Sign Up</a>}
-  </div>
-); 
