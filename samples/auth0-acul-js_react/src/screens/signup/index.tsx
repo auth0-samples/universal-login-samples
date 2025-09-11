5c5b46c7ccbd595238872daef23983cbb77bb879
Nandan Bhat <nandan.bhat@okta.com>
2025-09-11 17:08:01 +0530

    chore: renaming screen folders as per the current convention

---
+++ samples/auth0-acul-js_react/src/screens/signup/index.tsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { useSignupManager } from './hooks/useSignupManager';
+import { useSignupForm } from './hooks/useSignupForm';
+import { Logo } from "../../components/Logo";
+import { Title } from './components/Title';
+import { LoginForm } from './components/LoginForm';
+import { FederatedLogin } from './components/FederatedLogin';
+import { Links } from './components/Links';
+import { ErrorMessages } from './components/ErrorMessages';
+
+const SignupScreen: React.FC = () => {
+  const { signupManager, handleSignup, handleSocialSignup } = useSignupManager();
+  const identifiers = signupManager.getEnabledIdentifiers();
+  const { emailRef, usernameRef, phoneNumberRef, passwordRef, captchaRef, getFormValues } = useSignupForm();
+
+  const [isUsernameValid, setIsUsernameValid] = useState(true);
+  const [isValid, setIsValid] = useState(true);
+  const [errors, setErrors] = useState<Array<{ code: string; message: string }>>([]);
+  const [passwordValidation, setPasswordValidation] = useState<Array<{ code: string; policy: string; isValid: boolean }>>([]); // NEW
+  const [hasTypedPassword, setHasTypedPassword] = useState(false); 
+
+  const onPasswordChange = (password: string) => {
+    if (!hasTypedPassword && password.length > 0) {
+    setHasTypedPassword(true);
+  }
+
+    const results = signupManager.validatePassword(password);
+    setPasswordValidation(results);
+
+    const failedRules = results.filter(r => !r.isValid);
+    setIsValid(failedRules.length === 0);
+    setErrors(failedRules.map(r => ({ code: r.code, message: r.policy })));
+  };
+
+  const onUsernameChange = (username: string) => {
+    console.log('Username changed:', username);
+    // const results = signupManager.validateUsername(username); 
+    // setIsUsernameValid(results.isValid);
+    // setErrors((prevErrors) => [
+    //   ...prevErrors.filter((error) => error.code !== 'username'),
+    //   ...(!results.isValid
+    //     ? results.errors.map((err) => ({
+    //         code: 'username',
+    //         message: err.message,
+    //       }))
+    //     : []),
+    // ]);
+  };
+
+  const onLoginClick = () => {
+    const { username, email, phoneNumber, password, captcha } = getFormValues();
+    const results = signupManager.validatePassword(password);
+    const failed = results.filter(rule => !rule.isValid);
+
+    const isValid = failed.length === 0;
+    const errors = failed.map(rule => ({
+      code: rule.code,
+      message: rule.policy
+    }));
+
+    setIsValid(isValid);
+    setErrors(errors);
+    if (!isValid || !isUsernameValid) return;
+
+    handleSignup(username, email, phoneNumber, password, captcha);
+  };
+
+  return (
+    <div className="prompt-container">
+      <Logo />
+      <Title screenTexts={signupManager.screen.texts!} />
+
+      <LoginForm
+        emailRef={emailRef}
+        usernameRef={usernameRef}
+        phoneNumberRef={phoneNumberRef}
+        passwordRef={passwordRef}
+        captchaRef={captchaRef}
+        isCaptchaAvailable={signupManager.screen.isCaptchaAvailable}
+        captchaImage={signupManager.screen.captchaImage!}
+        countryCode={signupManager.transaction.countryCode!}
+        countryPrefix={signupManager.transaction.countryPrefix!}
+        onLoginClick={onLoginClick}
+        isValid={isValid}
+        errors={errors}
+        identifiers={identifiers}
+        passwordValidation={passwordValidation} // NEW
+        onPasswordChange={onPasswordChange}     // NEW
+        hasTypedPassword={hasTypedPassword}
+        usernameValid={isUsernameValid}
+        onUsernameChange={onUsernameChange}
+      />
+
+      <FederatedLogin
+        connections={signupManager.transaction.alternateConnections!}
+        onFederatedLogin={handleSocialSignup}
+      />
+
+      {signupManager.screen.links && (
+        <Links loginLink={signupManager.screen.links.loginLink!} />
+      )}
+
+      {signupManager.transaction.hasErrors && signupManager.transaction.errors && (
+        <ErrorMessages errors={signupManager.transaction.errors!} />
+      )}
+    </div>
+  );
+};
+export default SignupScreen;
