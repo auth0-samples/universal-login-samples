597c8a73be763f8086f40a2e433b0d7e36a03927
ankitatripathi.mp@gmail.com <ankitatripathi.mp@gmail.com>
2025-10-01 20:06:20 +0530

    Update signup-id example

--- samples/auth0-acul-js_react/src/screens/signup-id/index.tsx
+++ samples/auth0-acul-js_react/src/screens/signup-id/index.tsx
@@ -13,12 +13,12 @@ const SignupIdScreen: React.FC = () => {
 
   const signupIdManager = new SignupId();
 
+  const { isValid, errors } = signupIdManager.validateUsername(username);
+
   const title = signupIdManager.screen.texts?.title || '';
   const description = signupIdManager.screen.texts?.description || '';
   const federatedConnections = signupIdManager.transaction.alternateConnections ?? [];
   const links = signupIdManager.screen.links ?? {};
-  const [isValid, setIsValid] = useState(false);
-  const [errors, setErrors] = useState<{ code: string; message: string }[]>([]);
 
   useEffect(() => {
     const enabledIds = signupIdManager.getSignupIdentifiers();
@@ -26,12 +26,6 @@ const SignupIdScreen: React.FC = () => {
   }, []);
 
 
-  const handleUsernameChange = (value: string) => {
-    setUsername(value);
-    const { isValid, errors: usernameError } = signupIdManager.validateUsername(value);
-    setIsValid(isValid);
-    setErrors(usernameError);
-  };
   const handleSignup = async (e: React.FormEvent) => {
     e.preventDefault();
     setError('');
@@ -58,7 +52,7 @@ const SignupIdScreen: React.FC = () => {
       setError(errors[0].message);
       return;
     }
-
+    
     try {
       await signupIdManager.signup({
         email,
@@ -128,7 +122,7 @@ const SignupIdScreen: React.FC = () => {
                 id="username"
                 placeholder="Enter your username"
                 value={username}
-                onChange={(e) => handleUsernameChange(e.target.value)}
+                onChange={(e) => setUsername(e.target.value)}
                 className={`input-field ${username && !isValid ? 'border-red-500' : 'border-gray-300'
                   }`}
               />
