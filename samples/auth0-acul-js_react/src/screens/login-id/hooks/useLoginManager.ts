7f6743bd21e7ca963ed0383a9a4120744f140727
Nandan Bhat <nandan.bhat@okta.com>
2025-10-13 12:36:37 +0530

    Added 'registerPasskeyAutofill' utility usage

--- samples/auth0-acul-js_react/src/screens/login-id/hooks/useLoginManager.ts
+++ samples/auth0-acul-js_react/src/screens/login-id/hooks/useLoginManager.ts
@@ -1,9 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LoginIdInstance from "@auth0/auth0-acul-js/login-id";
 
 export const useLoginManager = () => {
   const [loginManager] = useState(() => new LoginIdInstance());
 
+  useEffect(() => {
+    (async () => {
+      await loginManager.registerPasskeyAutofill('username');
+    })();
+  }, []);
+    
   const handleLogin = (username: string, captcha: string): void => {
     const options = {
       username,
